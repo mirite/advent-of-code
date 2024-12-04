e907c34298bda03d33cae2dda76a59ab20676af0
Jesse Conner <me@jesseconner.ca>
2024-12-04 18:13:23 -0500

    Day 2 (part 2)

--- day2-part2.ts
+++ day2-part2.ts
@@ -2,14 +2,26 @@ import fs from "node:fs";
 const rows = fs.readFileSync("day2-data.txt").toString().split("\n").filter(Boolean)
 const reports = rows.map(row => row.split(" ").map(v => Number.parseInt(v)));
 let sum = 0;
-loop1: for (const row of reports) {
+
+function isSafeReport(row: number[]): boolean {
 	const ascending = row[1] > row[0];
-	for (let i = 1; i < row.length; i++) {
+	for (let i = 0; i < row.length; i++) {
 		if ((ascending === true && row[i] < row[i - 1]) || (ascending === false && row[i] > row[i - 1]) || Math.abs(row[i] - row[i - 1]) > 3 || Math.abs(row[i] - row[i - 1]) < 1) {
-			continue loop1;
+			return false;
 		}
 	}
-	sum++;
+	return true;
+
+}
+
+outer: for (const row of reports) {
+
+	for (let i = 0; i < row.length; i++) {
+		if (isSafeReport([...row.slice(0, i), ...row.slice(i + 1)])) {
+			sum++;
+			continue outer;
+		}
+	}
 }
 console.log({ sum, total: rows.length });
 
