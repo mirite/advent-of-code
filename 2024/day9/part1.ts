9303390bded0b7927e02100d1bb5501e4f9b71f8
Jesse Conner <me@jesseconner.ca>
2024-12-10 20:54:41 -0500

    Rework algo to take multi-digit ids into account

--- 2024/day9/part1.ts
+++ 2024/day9/part1.ts
@@ -1,9 +1,10 @@
 import fs from "node:fs";
 const testing = true;
 
-function parse(raw: string): string {
+function parse(raw: string): { parsed: string; lastID: number } {
 	let output = "";
-	for (let i = 0; i < raw.length; i++) {
+	let i = 0;
+	for (; i < raw.length; i++) {
 		const curr = Number.parseInt(raw[i]);
 		if (i % 2 === 1) {
 			for (let n = 0; n < curr; n++) output += ".";
@@ -11,12 +12,45 @@ function parse(raw: string): string {
 			for (let n = 0; n < curr; n++) output += `${i / 2}`;
 		}
 	}
+	return { parsed: output, lastID: Math.floor((i - 1) / 2) };
+}
+
+function defrag(parsed: string, lastID: number): string {
+	let output = "";
+	let i = 0;
+	let j = parsed.length;
+	for (; i < j; i++) {
+		if (parsed[i] !== ".") {
+			output += parsed[i];
+		} else {
+			for (; j > i; j--) {
+				if (parsed[j - 1] === ".") continue;
+				const blockSize = String(lastID).length;
+				const block = parsed.substring(j - blockSize, j);
+				output += block;
+				console.log({ block, j, blockSize, lastID });
+				j -= blockSize;
+				const peek = parsed.substring(j - blockSize, blockSize);
+				break;
+			}
+		}
+	}
 	return output;
 }
+
+function getCheckSum(defragged: string): number {
+	let sum = 0;
+	for (let i = 0; i < defragged.length; i++)
+		sum += i * Number.parseInt(defragged[i]);
+	return sum;
+}
+
 (function () {
 	const raw = fs
 		.readFileSync(testing ? "sample-data.txt" : "data.txt")
 		.toString();
-	const parsed = parse(raw);
-	console.log(parsed);
+	const { parsed, lastID } = parse(raw);
+	const defragged = defrag(parsed, lastID);
+	console.log(defragged);
+	console.log(getCheckSum(defragged));
 })();
